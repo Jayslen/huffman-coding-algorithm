e7f7d8a075b1acd39d4c5e27a509e122655d9044
Jayslen <jayslen134@gmail.com>
2026-08-06 20:36:13 -0400

    Extracted char frequency

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
+use std::collections::HashMap;
 use std::env;
-use std::io::{BufRead, Read};
-use std::{fs, io};
+use std::fs;
+use std::io::Read;
+
+//enum node_value
+
+#[derive(Debug)]
+struct LeafNode {
+    frequency: usize,
+    char: String,
+    left: Option<Box<LeafNode>>,
+    right: Option<Box<LeafNode>>,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
     let mut file = fs::File::open(file_path).unwrap();
 
+    let mut hash_map: HashMap<String, usize> = HashMap::new();
+    let heap: Vec<LeafNode> = Vec::new();
     let mut buffer: [u8; 2024] = [0; 2024];
 
     loop {
@@ -17,9 +30,20 @@ fn main() {
         let chunk = &buffer[..bytes_read];
 
         match std::str::from_utf8(chunk) {
-            Ok(text) => print!("{}", text),
+            Ok(text) => {
+                let s: Vec<_> = text.split("").collect();
+                for char in s {
+                    let value = char.to_string();
+                    if let Some(v) = hash_map.get(&value) {
+                        hash_map.insert(value, v + 1);
+                    } else {
+                        hash_map.insert(value, 1);
+                    }
+                }
+            }
             Err(_) => println!("{:?}", chunk),
         }
-        break;
     }
+    build_heap(&hash_map);
+    //println!("{:?}", hash_map);
 }
