279ce7e52f08700778e6c83134fbc1c90a03e37f
Jayslen <jayslen134@gmail.com>
2026-08-11 19:25:25 -0400

    feat: first implementation of building huffman tree

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,9 @@ fn main() {
             Ok(text) => {
                 let s: Vec<_> = text.split("").collect();
                 for char in s {
+                    if char.len() == 0 {
+                        continue;
+                    }
                     let value = char.to_string();
                     if let Some(v) = hash_map.get(&value) {
                         hash_map.insert(value, v + 1);
@@ -45,8 +48,10 @@ fn main() {
         }
     }
     build_heap(&hash_map, &mut heap);
-    //println!("{:?}", hash_map);
+    build_tree(&mut heap);
+    println!("{:#?}", heap);
 }
+
 fn build_heap(map: &HashMap<String, usize>, heap: &mut Vec<LeafNode>) {
     for i in map {
         heap.push(LeafNode {
@@ -85,5 +90,62 @@ fn heapify(heap: &mut Vec<LeafNode>, node_idx: usize, n: usize) {
     if smallest != node_idx {
         heap.swap(node_idx, smallest);
         heapify(heap, smallest, n);
+    }
+}
+
+fn build_tree(heap: &mut Vec<LeafNode>) {
+    while heap.len() > 1 {
+        let root = heap[0].clone();
+        let smallest = if heap.len() == 2 {
+            1
+        } else if &heap[(0 * 2) + 1].frequency > &heap[(0 * 2) + 2].frequency {
+            (0 * 2) + 1
+        } else {
+            (0 * 2) + 2
+        };
+
+        let second = heap[smallest].clone();
+
+        delete(heap, &root.char);
+        delete(heap, &second.char);
+
+        let internal_node = LeafNode {
+            frequency: root.frequency + second.frequency,
+            char: format!("{}{}", root.char, second.char),
+            left: Some(Box::new(root)),
+            right: Some(Box::new(second)),
+        };
+
+        heap.push(internal_node);
+
+        // Loop
+        // Identify first 2
+        // Sume them and add them to the heap
+        // Merge them into a new node and add it to the heap
+        // Remove the first 2 nodes from the heap
+        // Repeat until there is only 1 node left in the heap
+        //
+    }
+
+    fn delete(heap: &mut Vec<LeafNode>, value: &String) {
+        // Find index of value to be deleted
+        let mut index: i32 = -1;
+        for i in 0..heap.len() {
+            if heap[i].char == *value {
+                index = i as i32;
+                break;
+            }
+        }
+
+        if index == -1 {
+            return;
+        }
+
+        let len = heap.len() - 1;
+
+        heap.swap(index as usize, len);
+        heap.pop();
+
+        heapify(heap, index as usize, len);
     }
 }
